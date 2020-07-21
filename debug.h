58aa359e1444f835c1c3f8f1ea8ac5d95200d9f7
AlbertoBSD <alberto.bsd@gmail.com>
2020-07-21 16:29:10 +0200

    now with debug_realloc, debug_calloc, and new README.md

--- debug.h
+++ debug.h
@@ -1,5 +1,5 @@
 #pragma once
-#include<pthread.h> 
+//#include<pthread.h> 
 #include<string.h>
 #include<stdlib.h>
 
@@ -8,26 +8,30 @@
 #define DEBUG_INDEX 2
 #define DEBUG_MEMORY 3
 
-pthread_mutex_t debug_lock; 
+//pthread_mutex_t debug_lock; 
 
-char *debug_malloc(int length);
-void debug_free(char *ptr);
+void* debug_malloc(size_t size);
+void* debug_calloc(size_t nmemb,size_t size);
+void debug_free(void *ptr);
 
 void debug_optimizar();
-void debug_add(char *ptr,int length);
-void debug_remove(char *ptr);
+void debug_add(void *ptr,int length);
+int debug_remove(void *ptr);
 void debug_status();
 
 
-char **debug_ptr = NULL;
+void **debug_ptr = NULL;
 int *debug_ptr_sizes = NULL;
 int *debug_valores = NULL;
 
-char *debug_malloc(int length)	{
-	char *ptr = NULL;
-	ptr = (char*) malloc(length);
+/*
+ *	Custom malloc function this function save the ptr in a List to debug your code
+ */
+void* debug_malloc(size_t size)	{
+	void *ptr = NULL;
+	ptr = (char*) malloc(size);
 	if(ptr)	{
-		debug_add(ptr,length);
+		debug_add(ptr,size);
 	}
 	else	{
 		perror("malloc");
@@ -35,19 +39,45 @@ char *debug_malloc(int length)	{
 	return ptr;
 }
 
-void debug_free(char *ptr)	{
+void* debug_calloc(size_t nmemb,size_t size)	{
+	void *ptr = NULL;
+	ptr = (char*) calloc(nmemb,size);
 	if(ptr)	{
-		debug_remove(ptr);
-		free(ptr);
+		debug_add(ptr,size);
+	}
+	else	{
+		perror("calloc");
+	}
+	return ptr;
+}
+
+void* debug_realloc(void* ptr,size_t size)	{
+	void *ptr_new = NULL;
+	ptr_new = realloc(ptr,size);
+	if(ptr_new != NULL)	{
+		if(ptr_new != ptr)	{
+			debug_remove(ptr);
+			debug_add(ptr_new,size);
+		}
+	}else	{
+		perror("realloc");
+	}
+	return ptr_new;
+}
+
+void debug_free(void *ptr)	{
+	if(ptr)	{
+		if(debug_remove(ptr) == 0)
+			free(ptr);
 	}
 }
 
-void debug_add(char *ptr,int length)	{
-	pthread_mutex_lock(&debug_lock);
+void debug_add(void *ptr,int length)	{
+	//pthread_mutex_lock(&debug_lock);
 	if(debug_valores[DEBUG_INDEX] >= debug_valores[DEBUG_ITEMS_MAX])	{
 		debug_valores[DEBUG_ITEMS_MAX]*=2;
-		debug_ptr = realloc(debug_ptr,debug_valores[DEBUG_ITEMS_MAX]*sizeof(char*));
-		debug_ptr_sizes = realloc(debug_ptr_sizes,debug_valores[DEBUG_ITEMS_MAX]*sizeof(int));
+		debug_ptr = (void**)realloc((void*)debug_ptr,debug_valores[DEBUG_ITEMS_MAX]*sizeof(char*));
+		debug_ptr_sizes = (int*)realloc((void*)debug_ptr_sizes,debug_valores[DEBUG_ITEMS_MAX]*sizeof(int));
 	}
 	debug_ptr[debug_valores[DEBUG_INDEX]] = ptr;
 	debug_ptr_sizes[debug_valores[DEBUG_INDEX]] = length;
@@ -57,13 +87,18 @@ void debug_add(char *ptr,int length)	{
 		debug_optimizar();
 	}
 	debug_valores[DEBUG_INDEX]++;
-	pthread_mutex_unlock(&debug_lock);
+	//pthread_mutex_unlock(&debug_lock);
 }
 
-void debug_remove(char *ptr)	{
+/*
+ *	This function remove ptr internaly in our debug list
+ *  return 0 on success 
+ */
+int debug_remove(void *ptr)	{
+	int ret = 0;
 	int i = 0;
 	int encontrado = 0;
-	pthread_mutex_lock(&debug_lock);
+	//pthread_mutex_lock(&debug_lock);
 	while( !encontrado && i < debug_valores[DEBUG_INDEX] )	{
 		if(ptr == debug_ptr[i])	{
 			encontrado = 1;
@@ -75,9 +110,11 @@ void debug_remove(char *ptr)	{
 		i++;
 	}
 	if(encontrado == 0)	{
-		printf("Pointer not found %p ptr - you missing change some malloc, calloc realloc?\n",ptr	);
+		printf("Pointer not found 0x%p \nMay be you missing change some malloc, calloc realloc?\nOr pointer previously release\n",ptr	);
+		ret = 1;
 	}
-	pthread_mutex_unlock(&debug_lock);
+	//pthread_mutex_unlock(&debug_lock);
+	return ret;
 }
 
 void debug_optimizar()	{
@@ -100,18 +137,20 @@ void debug_init()	{
 	if(debug_ptr == NULL)	{
 		debug_valores = (int*) calloc(4,sizeof(int));
 		debug_valores[DEBUG_ITEMS_MAX] = 2;
-		debug_ptr = (char**)calloc(debug_valores[DEBUG_ITEMS_MAX],sizeof(char*));
+		debug_ptr = (void**)calloc(debug_valores[DEBUG_ITEMS_MAX],sizeof(char*));
 		debug_ptr_sizes = (int*)calloc(debug_valores[DEBUG_ITEMS_MAX],sizeof(char*));
 	}
+	/*
 	if(pthread_mutex_init(&debug_lock, NULL)  != 0)	{
 		perror("pthread_mutex_init");
 		exit(1);
 	}
+	*/
 }
 
 void debug_status()	{
-	pthread_mutex_lock(&debug_lock);
+	//pthread_mutex_lock(&debug_lock);
 	printf("Current memory Allocated: %i bytes\n",debug_valores[DEBUG_MEMORY]);
 	printf("Current total chunks used : %i\n",debug_valores[DEBUG_ITEMS]);
-	pthread_mutex_unlock(&debug_lock);
+	//pthread_mutex_unlock(&debug_lock);
 }
--- test.c
+++ test.c
@@ -5,14 +5,15 @@
 
 
 int main()	{
-	char *memoria;
+	char *m1,*m2,*m3;
 	debug_init();
-
-	memoria = debug_malloc(1024*1024);
-	
+	m1 = (char*)debug_malloc(1024*1024);
+	m2 = (char*)debug_calloc(1024*512,1);
+	m3 = (char*)debug_realloc((void*)m2,1024*1024);
 	/*
 		do something!
 	*/
 	debug_status();
+	debug_free(m2);	//m2 was reallocated this should show error unless  m2 == m3 was true
 	return 0;
 }
